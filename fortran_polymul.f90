bd1024d315192dec53cb622daed505bebdaf582c
Rafael Ferro <44897482+rafael-ferro@users.noreply.github.com>
2021-01-22 09:28:19 -0300

    Clear comments

--- fortran_polymul.f90
+++ fortran_polymul.f90
@@ -1,12 +1,5 @@
-!-----------------------------------------------------------------------
 subroutine polymul(n1, n2, p1, p2, p3)
-! Calculate pile-up according to Barradas & Reis XRS 35 (2006).
-!  Tw     shapping time (s)
-!  Tp     time required for the pulse to reach the maximum value
-!  time   total pulse time t (s)
-!  Nt     total pulse rate
-!  Tpulse = f*Tw, f depends on the amplifier (ref f=2)
-!-----------------------------------------------------------------------
+
     implicit none
 
     integer, parameter :: dp = kind(1.d0)
